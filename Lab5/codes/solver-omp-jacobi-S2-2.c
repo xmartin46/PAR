567cb6f2ebdd58b611b3ee8dc5577150e0985001
xmartin46 <xmartin46@gmail.com>
2019-06-07 18:15:00 +0200

    Lab5 S2 mirada una cosa

--- Lab5/codes/solver-omp-jacobi-S2-2.c
+++ Lab5/codes/solver-omp-jacobi-S2-2.c
@@ -16,27 +16,6 @@ void copy_mat (double *u, double *v, unsigned sizex, unsigned sizey)
  */
 double relax_jacobi (double *u, double *utmp, unsigned sizex, unsigned sizey)
 {
-    /*double diff, sum=0.0;
-  
-    int howmany=4;
-    for (int blockid = 0; blockid < howmany; ++blockid) {
-      int i_start = lowerb(blockid, howmany, sizex);
-      int i_end = upperb(blockid, howmany, sizex);
-      for (int i=max(1, i_start); i<= min(sizex-2, i_end); i++) {
-        for (int j=1; j<= sizey-2; j++) {
-	     utmp[i*sizey+j]= 0.25 * ( u[ i*sizey     + (j-1) ]+  // left
-	                               u[ i*sizey     + (j+1) ]+  // right
-				       u[ (i-1)*sizey + j     ]+  // top
-				       u[ (i+1)*sizey + j     ]); // bottom
-	     diff = utmp[i*sizey+j] - u[i*sizey + j];
-	     sum += diff * diff; 
-	 }
-      }
-    }
-
-    return sum;*/
-    
-    
 	double diff, sum=0.0;
 
 	int howmany = 4;
@@ -49,40 +28,13 @@ double relax_jacobi (double *u, double *utmp, unsigned sizex, unsigned sizey)
 			for (int j=1; j<= sizey-2; j++) {
 				 utmp[i*sizey+j]= 0.25 * ( u[ i*sizey     + (j-1) ]+  // left
 										   u[ i*sizey     + (j+1) ]+  // right
-							   u[ (i-1)*sizey + j     ]+  // top
-							   u[ (i+1)*sizey + j     ]); // bottom
+							   			   u[ (i-1)*sizey + j     ]+  // top
+							   			   u[ (i+1)*sizey + j     ]); // bottom
 				 diff = utmp[i*sizey+j] - u[i*sizey + j];
 				 sum += diff * diff; 
 			}
 		}
 	}
-
-    return sum;
-}
-
-/*
- * Blocked Gauss-Seidel solver: one iteration step
- */
-double relax_gauss (double *u, unsigned sizex, unsigned sizey)
-{
-    double unew, diff, sum=0.0;
-
-    int howmany=4;
-    for (int blockid = 0; blockid < howmany; ++blockid) {
-      int i_start = lowerb(blockid, howmany, sizex);
-      int i_end = upperb(blockid, howmany, sizex);
-      for (int i=max(1, i_start); i<= min(sizex-2, i_end); i++) {
-        for (int j=1; j<= sizey-2; j++) {
-	    unew= 0.25 * ( u[ i*sizey	+ (j-1) ]+  // left
-			   u[ i*sizey	+ (j+1) ]+  // right
-			   u[ (i-1)*sizey	+ j     ]+  // top
-			   u[ (i+1)*sizey	+ j     ]); // bottom
-	    diff = unew - u[i*sizey+ j];
-	    sum += diff * diff; 
-	    u[i*sizey+j]=unew;
-        }
-      }
-    }
 
     return sum;
 }
--- Lab5/codes/solver-omp-jacobi-S2-3.c
+++ Lab5/codes/solver-omp-jacobi-S2-3.c
@@ -16,30 +16,8 @@ void copy_mat (double *u, double *v, unsigned sizex, unsigned sizey)
  */
 double relax_jacobi (double *u, double *utmp, unsigned sizex, unsigned sizey)
 {
-    /*double diff, sum=0.0;
-  
-    int howmany=4;
-    for (int blockid = 0; blockid < howmany; ++blockid) {
-      int i_start = lowerb(blockid, howmany, sizex);
-      int i_end = upperb(blockid, howmany, sizex);
-      for (int i=max(1, i_start); i<= min(sizex-2, i_end); i++) {
-        for (int j=1; j<= sizey-2; j++) {
-	     utmp[i*sizey+j]= 0.25 * ( u[ i*sizey     + (j-1) ]+  // left
-	                               u[ i*sizey     + (j+1) ]+  // right
-				       u[ (i-1)*sizey + j     ]+  // top
-				       u[ (i+1)*sizey + j     ]); // bottom
-	     diff = utmp[i*sizey+j] - u[i*sizey + j];
-	     sum += diff * diff; 
-	 }
-      }
-    }
-
-    return sum;*/
-    
-    
 	double diff, sum=0.0;
 
-	
 	#pragma omp parallel reduction(+: sum) private(diff)
 	{
 		int howmany = omp_get_num_threads();
@@ -57,33 +35,6 @@ double relax_jacobi (double *u, double *utmp, unsigned sizex, unsigned sizey)
 			}
 		}
 	}
-
-    return sum;
-}
-
-/*
- * Blocked Gauss-Seidel solver: one iteration step
- */
-double relax_gauss (double *u, unsigned sizex, unsigned sizey)
-{
-    double unew, diff, sum=0.0;
-
-    int howmany=4;
-    for (int blockid = 0; blockid < howmany; ++blockid) {
-      int i_start = lowerb(blockid, howmany, sizex);
-      int i_end = upperb(blockid, howmany, sizex);
-      for (int i=max(1, i_start); i<= min(sizex-2, i_end); i++) {
-        for (int j=1; j<= sizey-2; j++) {
-	    unew= 0.25 * ( u[ i*sizey	+ (j-1) ]+  // left
-			   u[ i*sizey	+ (j+1) ]+  // right
-			   u[ (i-1)*sizey	+ j     ]+  // top
-			   u[ (i+1)*sizey	+ j     ]); // bottom
-	    diff = unew - u[i*sizey+ j];
-	    sum += diff * diff; 
-	    u[i*sizey+j]=unew;
-        }
-      }
-    }
 
     return sum;
 }
